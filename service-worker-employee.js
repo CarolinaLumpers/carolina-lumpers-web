7fe6158f8f68c13f420cab8064a81d0236dfffc0
Steve Garay <s.garay@carolinalumpers.com>
2025-10-14 15:09:02 -0400

    pwa

---
+++ service-worker-employee.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "cls-employee-v1";
+const ASSETS = [
+  "./employeeDashboard.html",
+  "./employee.js",
+  "./style.css",
+  "./manifest-employee.json",
+  "./assets/CLS-favicon.png"
+];
+
+// Install: cache assets
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+// Activate: clean old caches
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+// Fetch: serve cached or fetch new
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
