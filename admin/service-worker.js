9d89e312837365ffb19054b72e0c7321c3d859f9
Steve Garay <s.garay@carolinalumpers.com>
2025-10-14 14:45:16 -0400

    Fix PWA icon and service worker paths

--- admin/service-worker.js
+++ admin/service-worker.js
@@ -1,14 +1,31 @@
-﻿const CACHE_NAME = "cls-admin-v1";
+﻿const CACHE_NAME = "cls-admin-v2";
 const ASSETS = [
   "./adminDashboard.html",
   "./style.css",
   "./admin.js",
   "./manifest.json",
-  "../assets/CLS-favicon.png"
+  "./assets/CLS-favicon.png"
 ];
+
+// Install event
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
+
+// Activate event: remove old caches automatically
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+// Fetch event
 self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
